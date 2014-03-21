d59d36d9e2a980cdeefce3ffd4f643ca2e571be0
Valentin Kuznetsov <vkuznet@gmail.com>
2014-03-21 11:58:38 -0400

    Add some improvements

--- install_deps.sh
+++ install_deps.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+
+# make clean environment
+unset PYTHONPATH
+
+# download virtualenv and install pip
 venv=virtualenv-1.11.4
 curl -O https://pypi.python.org/packages/source/v/virtualenv/$venv.tar.gz
 tar xfz $venv.tar.gz
 python $venv/virtualenv.py soft
 . soft/bin/activate
+
+# now we're ready to go
 export PATH=$PWD/soft/bin:$PATH
 pip install cherrypy
 pip install Cheetah
