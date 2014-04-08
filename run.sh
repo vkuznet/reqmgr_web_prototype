09ece6dc8c8dcf62df5a8eee3306c39d7719b191
Valentin Kuznetsov <vkuznet@gmail.com>
2014-04-08 11:10:23 -0400

    Use setup.sh if it is present

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-export PATH=$PWD/soft/bin:$PATH
-export PYTHONPATH=$PWD/python
+if [ -f "setup.sh" ]; then
+    source $PWD/setup.sh
+else
+    export PATH=$PWD/soft/bin:$PATH
+fi
+export PYTHONPATH=$PWD/python:$PYTHONPATH
 export IMG_ROOT=$PWD/images
 export CSS_ROOT=$PWD/css
 export JS_ROOT=$PWD/js
